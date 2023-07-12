2368da6b265102573719f32017ca8c32d46997f6
Afriandiii <rahmanaafriandii@gmail.com>
2023-07-12 17:32:36 +0700

    update

--- Peminjam.java
+++ Peminjam.java
@@ -1,28 +1,40 @@
-public abstract class Peminjam implements UserLogin {
+public  abstract class Peminjam implements UserLogin{
     private boolean isLogin;
+    private boolean isAntiDenda;
 
     @Override
-    public void doLogin() {
-        System.out.println("Peminjam berhasil login");
+    public void doLogin(){
+        System.out.println("Peminjam Berhasil Login");
         isLogin = true;
     }
 
     @Override
-    public void doLogout() {
+    public void doLogout(){
         System.out.println("Peminjam berhasil logout");
         isLogin = false;
     }
 
     @Override
-    public boolean isLogin() {
+    public boolean isLogin(){
         return isLogin;
     }
 
     public abstract void setKodePeminjam(String kodePeminjam);
-
     public abstract String getKodePeminjam();
-
     public abstract void setNamaPeminjam(String namaPeminjam);
-
     public abstract String getNamaPeminjam();
+
+    public void addAntiDenda (){
+
+    }
+        
+    public void removeAntiDenda(){
+        
+    }
+
+    public boolean getAntiDenda(){
+        return isAntiDenda;
+    }
+
+
 }
